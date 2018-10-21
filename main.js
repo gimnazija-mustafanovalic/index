dbfc40844a2b5de8567ea4ed1f7ccf529abeb13e
gimnazija-mustafanovalic <44281860+gimnazija-mustafanovalic@users.noreply.github.com>
2018-10-21 17:34:12 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+document.getElementById('menuIcon').addEventListener('click', function() {
+  var icon = document.getElementById('menuIcon');
+  var menu = document.getElementById('menu');
+  menu.classList.toggle('menuOpen');
+  if (menu.classList.contains('menuOpen')) {
+    icon.classList = 'fas fa-times';
+  } else {
+    icon.classList = 'fas fa-bars';
+  }
+})
